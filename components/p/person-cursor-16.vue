d171529f65f4508cd12809162ce94a01db52f5b4
Vjacheslav Trushkin <cyberalien@gmail.com>
2026-09-20 13:27:50 +0300

    chore: update files

---
+++ components/p/person-cursor-16.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { Icon } from '@iconify/css-vue';
+import '../../css/z/zror_sbrn.css';
+import '../../css/c/c_5bp2b7l.css';
+
+const props = defineProps(["width","height"]);
+
+const viewBox = {"width":16,"height":16};
+const content = `<path class="zror_sbrn"/><path class="c_5bp2b7l"/>`;
+</script>
+<template><Icon :width="width" :height="height" :viewBox="viewBox" :content="content" fallback="octicon:person-cursor-16" /></template>
---
+++ components/p/person-cursor-24.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { Icon } from '@iconify/css-vue';
+import '../../css/y/yz47_7v9x.css';
+import '../../css/n/n9qbr_l5i.css';
+
+const props = defineProps(["width","height"]);
+
+const viewBox = {"width":24,"height":24};
+const content = `<path class="yz47_7v9x"/><path class="n9qbr_l5i"/>`;
+</script>
+<template><Icon :width="width" :height="height" :viewBox="viewBox" :content="content" fallback="octicon:person-cursor-24" /></template>

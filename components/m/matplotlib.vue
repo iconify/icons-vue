f98d07001846cd89c3330ec56e9e47dc7b876d48
Vjacheslav Trushkin <cyberalien@gmail.com>
2026-09-20 13:39:33 +0300

    chore: update files

--- components/m/matplotlib.vue
+++ components/m/matplotlib.vue
@@ -3,10 +3,10 @@ import { Icon } from '@iconify/css-vue';
 import '../../css/t/taivy3b5v.css';
 import '../../css/v/vgor86mrc.css';
 import '../../css/h/h92mbtbzf.css';
-import '../../css/g/g64mxfb1r.css';
-import '../../css/q/qyeron-oi.css';
-import '../../css/k/kdsagmdnv.css';
-import '../../css/z/zhd6k28of.css';
+import '../../css/z/zw1nn0bfe.css';
+import '../../css/d/dc-hdab0j.css';
+import '../../css/k/koejanb8m.css';
+import '../../css/g/gk-ctjbtm.css';
 import '../../css/p/ph9i1ny2q.css';
 import '../../css/x/xert7clhm.css';
 import '../../css/k/k8rbq_y3x.css';
@@ -26,6 +26,6 @@ import '../../css/l/ldzuwm3pp.css';
 const props = defineProps(["width","height"]);
 
 const viewBox = {"width":128,"height":128};
-const content = `<path class="taivy3b5v"/><path class="vgor86mrc"/><g class="h92mbtbzf"><path class="g64mxfb1r"/><path class="qyeron-oi"/><path class="kdsagmdnv"/><path class="zhd6k28of"/></g><path class="ph9i1ny2q"/><path class="xert7clhm"/><path class="k8rbq_y3x"/><path class="h8j31qdcq"/><path class="s-aoj2beg"/><path class="y6yeeab2g"/><path class="ds_l5pn8i"/><path class="h81l3q0tf"/><path class="jqq5e_b9v"/><path class="aei-rcctv"/><path class="qazaucknc"/><path class="i794uojlu"/><path class="l7w-8bc5i"/><path class="yxgtayp_v"/><path class="ldzuwm3pp"/>`;
+const content = `<path class="taivy3b5v"/><path class="vgor86mrc"/><g class="h92mbtbzf"><path class="zw1nn0bfe"/><path class="dc-hdab0j"/><path class="koejanb8m"/><path class="gk-ctjbtm"/></g><path class="ph9i1ny2q"/><path class="xert7clhm"/><path class="k8rbq_y3x"/><path class="h8j31qdcq"/><path class="s-aoj2beg"/><path class="y6yeeab2g"/><path class="ds_l5pn8i"/><path class="h81l3q0tf"/><path class="jqq5e_b9v"/><path class="aei-rcctv"/><path class="qazaucknc"/><path class="i794uojlu"/><path class="l7w-8bc5i"/><path class="yxgtayp_v"/><path class="ldzuwm3pp"/>`;
 </script>
 <template><Icon :width="width" :height="height" :viewBox="viewBox" :content="content" fallback="thesvg-color:matplotlib" /></template>

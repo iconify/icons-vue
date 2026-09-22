1e4b79858116822e045906cef3f6542c76b3d4de
Vjacheslav Trushkin <cyberalien@gmail.com>
2026-09-22 15:38:45 +0300

    chore: update files

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # @iconify-vue/lucide
 
+⚠️ This is an alternative repository. Main repository has moved to [GitLab](https://gitlab.com/iconify-icons/vue/-/tree/lucide).
 **Lucide**
 
 Author: [Lucide Contributors](https://github.com/lucide-icons/lucide)
@@ -10,6 +11,11 @@ Browse all icons: [preview Lucide on Iconify](https://icon-sets.iconify.design/l
 
 ## Installation
 
+```bash
+npm install gitlab:iconify-icons/vue#lucide
+```
+
+or
 ```bash
 npm install github:iconify/icons-vue#lucide
 ```
--- components/m/mail-pen.vue
+++ components/m/mail-pen.vue
@@ -1,12 +1,12 @@
 <script setup>
 import { Icon } from '@iconify/css-vue';
 import '../../css/n/nrj6p8qat.css';
-import '../../css/y/y4nx2qbry.css';
+import '../../css/g/g7f0tg48i.css';
 import '../../css/m/m4czh3vxz.css';
 
 const props = defineProps(["width","height"]);
 
 const viewBox = {"width":24,"height":24};
-const content = `<g class="nrj6p8qat"><path class="y4nx2qbry"/><path class="m4czh3vxz"/></g>`;
+const content = `<g class="nrj6p8qat"><path class="g7f0tg48i"/><path class="m4czh3vxz"/></g>`;
 </script>
 <template><Icon :width="width" :height="height" :viewBox="viewBox" :content="content" fallback="lucide:mail-pen" /></template>

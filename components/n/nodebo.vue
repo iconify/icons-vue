07e3bb8c56b250b9819f754d0fa6246dda1fbb0b
Vjacheslav Trushkin <cyberalien@gmail.com>
2026-09-23 16:20:10 +0300

    chore: update files

---
+++ components/n/nodebo.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { Icon } from '@iconify/css-vue';
+import '../../css/j/ja77f2b5e.css';
+import '../../css/s/sb-7x_b3r.css';
+import '../../css/f/fzmbfkb6j.css';
+
+const props = defineProps(["width","height"]);
+
+const viewBox = {"width":120,"height":120};
+const content = `<g class="ja77f2b5e"><circle class="sb-7x_b3r"/><path class="fzmbfkb6j"/></g>`;
+</script>
+<template><Icon :width="width" :height="height" :viewBox="viewBox" :content="content" fallback="thesvg:nodebo" /></template>
--- components/o/okta.vue
+++ components/o/okta.vue
@@ -1,10 +1,10 @@
 <script setup>
 import { Icon } from '@iconify/css-vue';
-import '../../css/b/b07s0zb2q.css';
+import '../../css/o/oqm9wbc2m.css';
 
 const props = defineProps(["width","height"]);
 
-const viewBox = {"width":24,"height":24};
-const content = `<path class="b07s0zb2q"/>`;
+const viewBox = {"width":33,"height":33};
+const content = `<path clip-rule="evenodd" class="oqm9wbc2m"/>`;
 </script>
 <template><Icon :width="width" :height="height" :viewBox="viewBox" :content="content" fallback="thesvg:okta" /></template>

248898703db282d84ea096da51be125888133414
Vjacheslav Trushkin <cyberalien@gmail.com>
2026-09-26 14:42:57 +0300

    chore: update files

---
+++ components/i/icinga.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { Icon } from '@iconify/css-vue';
+import '../../css/i/ipq8xlbpf.css';
+
+const props = defineProps(["width","height"]);
+
+const viewBox = {"width":128,"height":128};
+const content = `<path class="ipq8xlbpf"/>`;
+</script>
+<template><Icon :width="width" :height="height" :viewBox="viewBox" :content="content" fallback="devicon:icinga" /></template>
